8c0f78fdb6d4c538dedf1edae3e112b70eb2a992
Elvis Knapman <elvis@elvisknapman.com>
2020-10-21 17:44:08 -0400

    renamed to popularMovies

---
+++ src/actionCreators/popularMovies.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import {
+  FETCH_POPULAR_MOVIES_START,
+  FETCH_POPULAR_MOVIES_SUCCESS,
+  FETCH_POPULAR_MOVIES_FAILURE,
+} from '../actionTypes';
+
+export const popularMovies = () => async (dispatch) => {
+  dispatch({ type: FETCH_POPULAR_MOVIES_START });
+
+  try {
+    const result = await axios.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=9bf58527f8639b438523290ccf8faf71'
+    );
+    // array of movies is nested in object (in data.results)
+    dispatch({
+      type: FETCH_POPULAR_MOVIES_SUCCESS,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_POPULAR_MOVIES_FAILURE,
+      payload: 'There was an error fetching the movies. Please try again.',
+    });
+  }
+};
