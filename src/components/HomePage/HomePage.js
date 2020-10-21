d610d5230d1245dbfe56645228a711e15e886fcc
Elvis Knapman <elvis@elvisknapman.com>
2020-10-21 16:05:11 -0400

    Initial commit

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import TrendingMoviesContainer from '../TrendingMovies/TrendingMoviesContainer';
+
+const HomePage = (props) => {
+  return (
+    <>
+      <h1>The Home Page</h1>
+      <TrendingMoviesContainer />
+    </>
+  );
+};
+
+export default HomePage;
