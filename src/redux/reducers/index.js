0d56546aa37ac57f08dff92498a1bea9f204718a
Elvis Knapman <elvis@elvisknapman.com>
2020-10-23 15:39:44 -0400

    imported two reducers...
    imported topRatedMovies reducer and visibilityFilter reducer.. added them to the combineReducers export to be added to the store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 
 // reducers
 import popularMovies from './popularMovies';
+import topRatedMovies from './topRatedMovies';
 import searchMovies from './searchMovies';
+import visibilityFilter from './visibilityFilter';
 
 export default combineReducers({
+  visibilityFilter,
   popularMovies,
+  topRatedMovies,
   searchMovies,
 });
