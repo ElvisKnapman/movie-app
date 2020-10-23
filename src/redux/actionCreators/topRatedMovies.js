2e78bddb02e4b3868358002cffd9af444a1f5fe4
Elvis Knapman <elvis@elvisknapman.com>
2020-10-23 15:35:41 -0400

    action creator to fetch top rated movies..
    sends results to reducer and state is updated accordingly

---
+++ src/redux/actionCreators/topRatedMovies.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+import {
+  FETCH_TOP_RATED_MOVIES_START,
+  FETCH_TOP_RATED_MOVIES_SUCCESS,
+  FETCH_TOP_RATED_MOVIES_FAILURE,
+} from '../actionTypes';
+
+export const topRatedMovies = () => async (dispatch) => {
+  dispatch({ type: FETCH_TOP_RATED_MOVIES_START });
+
+  try {
+    const result = await axios.get(
+      'https://api.themoviedb.org/3/movie/top_rated?api_key=9bf58527f8639b438523290ccf8faf71'
+    );
+    const movies = result.data.results;
+    // send the movies to the reducer if successful
+    setTimeout(() => {
+      dispatch({ type: FETCH_TOP_RATED_MOVIES_SUCCESS, payload: movies });
+    }, 3000);
+    console.log('top rated movies', movies);
+  } catch (err) {
+    dispatch({
+      type: FETCH_TOP_RATED_MOVIES_FAILURE,
+      payload: 'There was an error retrieving the top rated movies.',
+    });
+  }
+};
