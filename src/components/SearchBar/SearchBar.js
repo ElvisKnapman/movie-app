dcc1359d04c1d9a4d1cf9f3030ba4e4d0bdf37c3
Elvis Knapman <elvis@elvisknapman.com>
2020-10-23 12:52:50 -0400

    added search feature...
    imported action to trigger the fetch of the searched movie and send to the store and reducer

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoMdSearch as SearchIcon } from 'react-icons/io';
+
+import { connect } from 'react-redux';
+
+import { searchMovies } from '../../redux/actionCreators/searchMovies';
 
 const SearchBar = (props) => {
   const [searchText, setSearchText] = useState('');
   const [isHover, setIsHover] = useState(false);
+
+  useEffect(() => {
+    // prevents action from firing on component mount... must have text in search box
+    if (searchText !== '') {
+      // trim leading and trailing whitespace and URI encode the string for the API query
+      const encodedString = encodeURI(searchText);
+      console.log('in the useEffect', encodedString);
+      console.log('search bar props', props);
+      props.searchMovies(encodedString);
+    }
+  }, [searchText]);
+
   const handleChange = (e) => {
     setSearchText(e.target.value);
   };
-
   console.log('search text', searchText);
 
   return (
@@ -15,7 +30,7 @@ const SearchBar = (props) => {
       <div className="search_bar">
         <input
           className="search_bar_input"
-          style={isHover ? { width: '50%' } : { width: '1rem' }}
+          style={isHover ? { width: '100%' } : { width: '1rem' }}
           name="movie_name"
           type="text"
           onChange={handleChange}
@@ -27,8 +42,19 @@ const SearchBar = (props) => {
           className="search_icon"
         />
       </div>
+      <div className="filter_container">
+        <div className="filter_option_container">
+          <span className="filter_option">Popular</span>
+        </div>
+        <div className="filter_option_container">
+          <span className="filter_option">Highest Rated</span>
+        </div>
+        <div className="filter_option_container">
+          <span className="filter_option">Trending</span>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default SearchBar;
+export default connect(null, { searchMovies })(SearchBar);
