194cfcbcbbe12a5b81aefb9bf76bce921a71c2ac
Elvis Knapman <elvis@elvisknapman.com>
2020-10-23 12:55:18 -0400

    added Link component to link to movie route

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = ({ movie }) => {
   return (
-    <div className="movie_card">
-      <img
-        className="movie_card_poster"
-        src={`http://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
-        alt={`Poster for the movie ${movie.title}`}
-      />
-    </div>
+    <Link to={`/movies/${movie.id}`}>
+      <div>
+        <img
+          className="movie_card_poster"
+          src={`http://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
+          alt={`Poster for the movie ${movie.title}`}
+        />
+      </div>
+    </Link>
   );
 };
 
