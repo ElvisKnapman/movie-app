8edf7b02bf6be048c35e81778c12b2c61e34df69
Elvis Knapman <elvis@elvisknapman.com>
2020-10-24 15:15:41 -0400

    imported No Poster image...
    added cternary to display poster if it has one or to display the placeholder image if the movie does not have a poster

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// placeholder image for no poster
+import NoPoster from '../../img/poster_placeholder.png';
+
 const MovieCard = ({ movie }) => {
+  // get movie poster to display or display placeholder
+  const hasPoster = movie.poster_path ? true : false;
+  // const pathPrefix = "http://image.tmdb.org/t/p/w500/";
+  // const posterPath = movie.poster.path ? movie.poster.path : {NoPoster};
   return (
     <Link to={`/movies/${movie.id}`}>
       <div>
         <img
           className="movie_card_poster"
-          src={`http://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
+          src={
+            hasPoster
+              ? `http://image.tmdb.org/t/p/w500/${movie.poster_path}`
+              : `${NoPoster}`
+          }
           alt={`Poster for the movie ${movie.title}`}
         />
       </div>
