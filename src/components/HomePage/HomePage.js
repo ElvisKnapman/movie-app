83cc13ce16480d91eaa5b9c26f9eff83386cb801
Elvis Knapman <elvis@elvisknapman.com>
2020-10-24 15:14:34 -0400

    added components to conditionally render...
    use visibilityFilter redux state variables to conditionally render the appropriate movie display component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,56 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 // components
-import TrendingMoviesContainer from '../TrendingMovies/TrendingMoviesContainer';
+import SearchBar from '../SearchBar/SearchBar';
+import PopularMovies from '../DisplayMovies/PopularMovies';
+import TopRatedMovies from '../DisplayMovies/TopRatedMovies';
+import TrendingMovies from '../DisplayMovies/TrendingMovies';
+import SearchedMovies from '../DisplayMovies/SearchedMovies';
+import ComponentContainer from '../ComponentContainer/ComponentContainer';
 
 const HomePage = (props) => {
   return (
-    <>
+    <div className="inner_page_container">
       <h1>The Home Page</h1>
-      <TrendingMoviesContainer />
-    </>
+      <ComponentContainer>
+        <SearchBar />
+      </ComponentContainer>
+      {props.showPopular && (
+        <ComponentContainer styles={{ minHeight: '100vh' }}>
+          <PopularMovies />
+        </ComponentContainer>
+      )}
+
+      {props.showTopRated && (
+        <ComponentContainer styles={{ minHeight: '100vh' }}>
+          <TopRatedMovies />
+        </ComponentContainer>
+      )}
+
+      {props.showTrending && (
+        <ComponentContainer styles={{ minHeight: '100vh' }}>
+          <TrendingMovies />
+        </ComponentContainer>
+      )}
+
+      {props.showSearched && (
+        <ComponentContainer styles={{ minHeight: '100vh' }}>
+          <SearchedMovies />
+        </ComponentContainer>
+      )}
+    </div>
   );
 };
 
-export default HomePage;
+const mapStateToProps = (state) => {
+  return {
+    showPopular: state.visibilityFilter.showPopular,
+    showTopRated: state.visibilityFilter.showTopRated,
+    showTrending: state.visibilityFilter.showTrending,
+    showSearched: state.visibilityFilter.showSearched,
+  };
+};
+
+export default connect(mapStateToProps, null)(HomePage);
