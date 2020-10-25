fafa27c971fbad9c900a154d744b522c71616d40
Elvis Knapman <elvis@elvisknapman.com>
2020-10-25 15:30:23 -0400

    imported TopBar to be rendered on all routes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './sass/index.scss';
 // components
 import HomePage from './components/HomePage/HomePage';
 import MovieDetailsPage from './components/MovieDetailsPage/MovieDetailsPage';
+import TopBar from './components/TopBar/TopBar';
 function App() {
   return (
     <Router>
       <div>
         <header>
+          <TopBar />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/movies/:id" component={MovieDetailsPage} />
