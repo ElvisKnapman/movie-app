01cb0a19b477b11dc804c34840bbe2241c83b465
Elvis Knapman <elvis@elvisknapman.com>
2020-10-25 18:32:12 -0400

    added alt tag to logo

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,7 +8,11 @@ const TopBar = () => {
       <span className="app_title">React Movie DB</span>
       <div className="powered_by_container">
         <span>Powered By</span>
-        <img src={TMDBLogo} className="tmdb_logo" />
+        <img
+          src={TMDBLogo}
+          className="tmdb_logo"
+          alt="Logo for the movie database"
+        />
       </div>
     </div>
   );
