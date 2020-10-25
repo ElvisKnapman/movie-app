a1817cbc357f24d9f9fb436298098230ef7c85ee
Elvis Knapman <elvis@elvisknapman.com>
2020-10-25 15:28:22 -0400

    top fixed bar on all pages

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import TMDBLogo from '../../img/themoviedblogo.svg';
+
+const TopBar = () => {
+  return (
+    <div class="top_bar_container">
+      <span className="app_title">React Movie DB</span>
+      <div className="powered_by_container">
+        <span>Powered By</span>
+        <img src={TMDBLogo} className="tmdb_logo" />
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
