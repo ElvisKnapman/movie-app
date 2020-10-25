c5f9b57888e9b60d5e44ee081d44070c00f5647c
Elvis Knapman <elvis@elvisknapman.com>
2020-10-25 18:08:48 -0400

    testing without redux

--- src/components/DisplayMovies/PopularMovies.js
+++ src/components/DisplayMovies/PopularMovies.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
+import axios from 'axios';
+
 // action creator(s)
 import { popularMovies as popularMoviesAction } from '../../redux/actionCreators/popularMovies';
 
@@ -10,23 +12,42 @@ import MovieCard from '../MovieCard/MovieCard';
 import LoadMoreMoviesButton from '../LoadMoreMoviesButton/LoadMoreMoviesButton';
 
 const PopularMovies = (props) => {
-  const { popularMoviesAction, popularMoviesList, isLoading } = props;
+  // const { popularMoviesAction, popularMoviesList, isLoading } = props;
 
   // current page of movie results
   const [currentPage, setCurrentPage] = useState(1);
+  const [popularMoviesList, setPopularMoviesList] = useState([]);
 
   useEffect(() => {
-    // fetch popular movies on component mount and when page number changes to load more movies -- pass page number in
-    popularMoviesAction(currentPage);
-  }, [popularMoviesAction, currentPage]);
+    const fetchMovies = async () => {
+      try {
+        const result = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=9bf58527f8639b438523290ccf8faf71&page=${currentPage}`
+        );
 
-  if (isLoading) {
-    return (
-      <div>
-        <LoadingSpinner />
-      </div>
-    );
-  }
+        const movieResults = result.data.results;
+        setPopularMoviesList([...popularMoviesList, ...movieResults]);
+        console.log(popularMoviesList);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchMovies();
+  }, [currentPage]);
+
+  // useEffect(() => {
+  //   // fetch popular movies on component mount and when page number changes to load more movies -- pass page number in
+  //   popularMoviesAction(currentPage);
+  //   console.log('rendered component');
+  // }, [currentPage]);
+
+  // if (isLoading) {
+  //   return (
+  //     <div>
+  //       <LoadingSpinner />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
@@ -47,17 +68,16 @@ const PopularMovies = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    popularMoviesList: state.popularMovies.movies,
-    isLoading: state.popularMovies.isLoading,
-    errorMessage: state.popularMovies.error || null,
-  };
-};
-const mapDispatchToProps = {
-  popularMoviesAction,
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     popularMoviesList: state.popularMovies.movies,
+//     isLoading: state.popularMovies.isLoading,
+//     errorMessage: state.popularMovies.error || null,
+//   };
+// };
+// const mapDispatchToProps = {
+//   popularMoviesAction,
+// };
 
-export default React.memo(
-  connect(mapStateToProps, mapDispatchToProps)(PopularMovies)
-);
+export default PopularMovies;
+// export default connect(mapStateToProps, mapDispatchToProps)(PopularMovies);
