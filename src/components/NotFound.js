ffd5b4b1f86afce09b611d21cee7dcb57c90fe24
Elvis Knapman <elvis@elvisknapman.com>
2020-10-26 15:02:17 -0400

    Not Found component

---
+++ src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <p
+        style={{
+          margin: '13rem auto 0',
+          width: '80%',
+          textAlign: 'center',
+          fontSize: '3rem',
+        }}>
+        Oops... the requested page could not be found.
+      </p>
+    </>
+  );
+};
+
+export default NotFound;
