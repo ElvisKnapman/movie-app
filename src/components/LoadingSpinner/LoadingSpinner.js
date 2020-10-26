a5009175c0f3c1c8afacd2ca459441beef2ff010
Elvis Knapman <elvis@elvisknapman.com>
2020-10-26 13:05:43 -0400

    removed className

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -5,13 +5,7 @@ import Loader from 'react-loader-spinner';
 const LoadingSpinner = () => {
   return (
     <div className="loading_spinner_container">
-      <Loader
-        type="Puff"
-        color="rgb(38, 55, 75)"
-        height={80}
-        width={80}
-        className="loading_spinner"
-      />
+      <Loader type="Puff" color="rgb(38, 55, 75)" height={80} width={80} />
     </div>
   );
 };
