e9b9ec75e9f73ad552225cf548afab20ceea93da
Elvis Knapman <elvis@elvisknapman.com>
2020-10-26 13:04:10 -0400

    removed axios import... (continued)
    removed unnecessary commented out code. Added loader spinner to button display conditional so a spinner is shown instead of the load more button when more movies are being loaded

--- src/components/DisplayMovies/PopularMovies.js
+++ src/components/DisplayMovies/PopularMovies.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 // action creator(s)
 import { popularMovies as popularMoviesAction } from '../../redux/actionCreators/popularMovies';
@@ -15,43 +14,13 @@ const PopularMovies = (props) => {
 
   // current page of movie results
   const [currentPage, setCurrentPage] = useState(1);
-  // const [popularMoviesList, setPopularMoviesList] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const getMovies = async () => {
-  //     // set is loading true
-  //     setIsLoading(true);
-  //     try {
-  //       const result = await axios.get(
-  //         `https://api.themoviedb.org/3/movie/popular?api_key=9bf58527f8639b438523290ccf8faf71&page=${currentPage}`
-  //       );
-
-  //       const movies = result.data.results;
-  //       setPopularMoviesList([...popularMoviesList, ...movies]);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //     // done action
-  //     setIsLoading(false);
-  //   };
-  //   getMovies();
-  // }, [currentPage]);
 
   console.log('the movies', popularMoviesList);
   useEffect(() => {
     // fetch popular movies on component mount and when page number changes to load more movies -- pass page number in
     popularMoviesAction(currentPage);
     console.log('rendered component');
-  }, [currentPage]);
-
-  // if (isLoading) {
-  //   return (
-  //     <div>
-  //       <LoadingSpinner />
-  //     </div>
-  //   );
-  // }
+  }, [currentPage, popularMoviesAction]);
 
   return (
     <div>
@@ -64,13 +33,15 @@ const PopularMovies = (props) => {
             ))}
         </div>
         {
-          // show button if not currently loading
+          // show button if not currently loading - else show loading spinner
           !isLoading ? (
             <LoadMoreMoviesButton
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
-          ) : null
+          ) : (
+            <LoadingSpinner />
+          )
         }
       </div>
     </div>
