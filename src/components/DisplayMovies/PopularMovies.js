a24000267db06cf37827733b180c5aa64e38017d
Elvis Knapman <elvis@elvisknapman.com>
2020-10-25 21:45:45 -0400

    removed redux to test loading

--- src/components/DisplayMovies/PopularMovies.js
+++ src/components/DisplayMovies/PopularMovies.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 // action creator(s)
-import { popularMovies as popularMoviesAction } from '../../redux/actionCreators/popularMovies';
+// import { popularMovies as popularMoviesAction } from '../../redux/actionCreators/popularMovies';
 
 // components
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
@@ -10,24 +11,47 @@ import MovieCard from '../MovieCard/MovieCard';
 import LoadMoreMoviesButton from '../LoadMoreMoviesButton/LoadMoreMoviesButton';
 
 const PopularMovies = (props) => {
-  const { popularMoviesAction, popularMoviesList, isLoading } = props;
+  // const { popularMoviesAction, popularMoviesList, isLoading } = props;
 
   // current page of movie results
   const [currentPage, setCurrentPage] = useState(1);
+  const [popularMoviesList, setPopularMoviesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // fetch popular movies on component mount and when page number changes to load more movies -- pass page number in
-    popularMoviesAction(currentPage);
-    console.log('rendered component');
+    const getMovies = async () => {
+      // set is loading true
+      setIsLoading(true);
+      try {
+        const result = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=9bf58527f8639b438523290ccf8faf71&page=${currentPage}`
+        );
+
+        const movies = result.data.results;
+        setPopularMoviesList([...popularMoviesList, ...movies]);
+      } catch (err) {
+        console.log(err);
+      }
+      // done action
+      setIsLoading(false);
+    };
+    getMovies();
   }, [currentPage]);
 
-  if (isLoading) {
-    return (
-      <div>
-        <LoadingSpinner />
-      </div>
-    );
-  }
+  console.log('the movies', popularMoviesList);
+  // useEffect(() => {
+  //   // fetch popular movies on component mount and when page number changes to load more movies -- pass page number in
+  //   popularMoviesAction(currentPage);
+  //   console.log('rendered component');
+  // }, [currentPage]);
+
+  // if (isLoading) {
+  //   return (
+  //     <div>
+  //       <LoadingSpinner />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
@@ -39,23 +63,29 @@ const PopularMovies = (props) => {
               <MovieCard movie={movie} key={index} />
             ))}
         </div>
-        <LoadMoreMoviesButton
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+        {
+          // show button if not currently loading
+          !isLoading ? (
+            <LoadMoreMoviesButton
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          ) : null
+        }
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    popularMoviesList: state.popularMovies.movies,
-    isLoading: state.popularMovies.isLoading,
-    errorMessage: state.popularMovies.error || null,
-  };
-};
-const mapDispatchToProps = {
-  popularMoviesAction,
-};
-export default connect(mapStateToProps, mapDispatchToProps)(PopularMovies);
+// const mapStateToProps = (state) => {
+//   return {
+//     popularMoviesList: state.popularMovies.movies,
+//     isLoading: state.popularMovies.isLoading,
+//     errorMessage: state.popularMovies.error || null,
+//   };
+// };
+// const mapDispatchToProps = {
+//   popularMoviesAction,
+// };
+export default PopularMovies;
+// export default connect(mapStateToProps, mapDispatchToProps)(PopularMovies);
